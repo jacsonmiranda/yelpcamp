6752d31c2e7a0ec338f8f62fd4eb2204c585fb85
Jacson Miranda <jacsonmiranda@gmail.com>
2019-01-26 13:18:33 +0000

    add mongolab database (app modifield)

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentsRoutes   = require("./routes/comments"),
     indexRoutes      = require("./routes/index")
 
 //mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect('mongodb://localhost:27017/yelp_camp_12 Deployed', { useNewUrlParser: true });
+//mongoose.connect('mongodb://localhost:27017/yelp_camp_12 Deployed', { useNewUrlParser: trsdfue });
+mongoose.connect('mongodb://jacsonmiranda:Jacson123@ds113495.mlab.com:13495/yelpcampdatabase', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
