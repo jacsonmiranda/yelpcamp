de2aa0c875e0467d37b2e6095481da1cc05e409d
Jacson Miranda <jacsonmiranda@gmail.com>
2019-01-27 10:51:21 +0000

    change to DATABASEURL locate the db

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ var express        = require("express"),
 var commentsRoutes   = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
-
+console.log(process.env.DATABASEURL);
 //mongoose.connect("mongodb://localhost/yelp_camp");
-//mongoose.connect('mongodb://localhost:27017/yelp_camp_12 Deployed', { useNewUrlParser: trsdfue });
-mongoose.connect('mongodb://jacsonmiranda:Jacson123@ds113495.mlab.com:13495/yelpcampdatabase', { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/yelp_camp_12_Deployed", { useNewUrlParser: true });
+//mongoose.connect('mongodb://jacsonmiranda:Jacson123@ds113495.mlab.com:13495/yelpcampdatabase', { useNewUrlParser: true });
+//mongoose.connect(process.env.DATABASEURL);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
